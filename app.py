44d3e17d6a528e251aeedb7c789ff550554a870a
safeekur rahman a.z. <158799961+safrah710@users.noreply.github.com>
2024-08-30 23:31:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 app = Flask(__name__)
-CORS(app)  # Apply CORS to your Flask app
+CORS(app)  
 
-# Load the spaCy model
+
 nlp = spacy.load('en_core_web_sm')
 
 def extract_summary(text, num_sentences=3):
     doc = nlp(text)
     sentences = [sent.text for sent in doc.sents]
-    
-    # If there are not enough sentences, return the original text
+
     if len(sentences) <= num_sentences:
         return text
 
@@ -33,15 +32,15 @@ def sentiment_analysis():
     data = request.json
     text = data.get('text', '')
 
-    # Using TextBlob
+
     blob = TextBlob(text)
     textblob_sentiment = blob.sentiment.polarity
 
-    # Using VADER
+
     analyzer = SentimentIntensityAnalyzer()
     vader_sentiment = analyzer.polarity_scores(text)['compound']
 
-    # Combine TextBlob and VADER results
+
     if textblob_sentiment > 0 and vader_sentiment > 0:
         sentiment = 'Positive'
     elif textblob_sentiment < 0 and vader_sentiment < 0:
@@ -57,7 +56,7 @@ def summarize_text():
     text = data.get('text', '')
     
     try:
-        summary = extract_summary(text, num_sentences=3)  # Summarize to 3 sentences
+        summary = extract_summary(text, num_sentences=3)  
     except Exception as e:
         summary = f"Error summarizing text: {str(e)}"
 
